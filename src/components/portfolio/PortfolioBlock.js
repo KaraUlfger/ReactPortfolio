9dae08823c07be91c49b311e8983e5d470c86f9f
Kara <128341148+KaraUlfger@users.noreply.github.com>
2024-05-21 19:59:20 -0700

    update

--- src/components/portfolio/PortfolioBlock.js
+++ src/components/portfolio/PortfolioBlock.js
@@ -1,24 +1,40 @@
 import React from 'react';
-import IconLink from "./IconLink";
-import {Box} from "@mui/material";
+import { Box } from "@mui/material";
 
 function PortfolioBlock(props) {
-   const {image, live, source, title} = props;
-   return (
-      <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
-         <Box component={'img'} src={image} alt={'mockup'}/>
-         <h1 style={{fontSize: '2rem'}}>{title}</h1>
-         <Box className={'portfolio'} display={'flex'} flexDirection={'column'} gap={'0.5rem'}
-              alignItems={'center'} fontSize={'1.5rem'} py={'2rem'}>
-            <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
-               <IconLink link={live} title={'Live Demo'} icon={'fa fa-safari'}/>
-            </Box>
-            <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
-               <IconLink link={source} title={'Source Code'} icon={'fa fa-code'}/>
-            </Box>
-         </Box>
-      </Box>
-   );
+    const { image, hoverImage, live } = props;
+    return (
+        <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
+            <a href={live} target="_blank" rel="noopener noreferrer" style={{ width: '100%', position: 'relative' }}>
+                <Box 
+                    sx={{
+                        position: 'relative',
+                        width: '100%',  // Set to 100% or a specific size
+                        height: 'auto',
+                        '&:hover .hover-image': {
+                            opacity: 1,
+                        },
+                        '& .hover-image': {
+                            position: 'absolute',
+                            top: 0,
+                            left: 0,
+                            width: '100%',
+                            height: '100%',
+                            opacity: 0,
+                            transition: 'opacity 0.3s ease',
+                        },
+                        '& .default-image': {
+                            width: '100%',
+                            height: '100%',
+                        }
+                    }}
+                >
+                    <img src={image} alt={'mockup'} className="default-image" />
+                    <img src={hoverImage} alt={'hover mockup'} className="hover-image" />
+                </Box>
+            </a>
+        </Box>
+    );
 }
 
 export default PortfolioBlock;
